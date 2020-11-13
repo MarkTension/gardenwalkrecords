2da4307e22279c3278d53280a65719d80c713212
MarkTension <tensen.mark@gmail.com>
2020-11-13 10:37:51 +0100

    Updates

---
+++ precache-manifest.39f9c7e01102de56ffa760e4964a8b6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401908d9ee93a30fa09ba4ecef62cf47",
+    "url": "/gardenwalkrecords/index.html"
+  },
+  {
+    "revision": "13a98a62f2b067aea094",
+    "url": "/gardenwalkrecords/static/css/main.ec2c0500.chunk.css"
+  },
+  {
+    "revision": "cebeaaa0f37575aa4327",
+    "url": "/gardenwalkrecords/static/js/2.e279f85b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/gardenwalkrecords/static/js/2.e279f85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a98a62f2b067aea094",
+    "url": "/gardenwalkrecords/static/js/main.b6cb4148.chunk.js"
+  },
+  {
+    "revision": "2b9d463d4f828f11136d",
+    "url": "/gardenwalkrecords/static/js/runtime-main.f3759e79.js"
+  },
+  {
+    "revision": "e4bfe799e5a3bbca89cfd52e7fb66785",
+    "url": "/gardenwalkrecords/static/media/m4.e4bfe799.png"
+  },
+  {
+    "revision": "9cd515ae2c033fb1597192cc5a77eb32",
+    "url": "/gardenwalkrecords/static/media/springwalkcollection.9cd515ae.png"
+  },
+  {
+    "revision": "57200ce868f52f60aac9f54f9df92664",
+    "url": "/gardenwalkrecords/static/media/stringPalooza2.57200ce8.png"
+  },
+  {
+    "revision": "82aa238e78abf2addbe1ed5dcad303f0",
+    "url": "/gardenwalkrecords/static/media/subconsciousGarden.82aa238e.png"
+  }
+]);
